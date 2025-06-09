244be1f2b1ea3238bf8ea1f4495bb961e1e1e725
DoCodeWithHarsh <harshal.margaj12@gmail.com>
2025-06-09 13:05:59 +0530

    fixes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -19,6 +19,7 @@ const Experience = () => {
 			<div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
 				{workExperience.map(card => (
 					<Button
+						key={card.id}
 						//   random duration will be fun , I think , may be not
 						duration={Math.floor(Math.random() * 10000) + 10000}
 						borderRadius="1.75rem"
